d20889fe62a6dcdfb4bfeec99971d85ae11d4288
Karen Nix <knn0010@auburn.edu>
2022-09-09 12:01:00 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import Quiz from './components/Quiz/Quiz';
+
+function App() {
+  return (
+    <Quiz />
+  );
+}
+
+export default App;
